3b43f1a42324003093bbc23fc335d1ed77415096
Steven-Shen1987 <steven246888@gmail.com>
2020-11-05 14:10:52 +1300

    message

--- src/app/testform/testform.component.ts
+++ src/app/testform/testform.component.ts
@@ -8,20 +8,16 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./testform.component.css']
 })
 export class TestformComponent implements OnInit {
-  
-  list:any
-  form = {
-    title:'',
-    body:'',
-  }
-  display:{}
+
+  list: any
+
 
   constructor(private http: HttpClient) { }
-  onSubmit(form: FormGroup){
+  onSubmit(form: FormGroup) {
     console.log(form)
 
-    if (form.valid && form.dirty){
-      this.display = form.value
+    if (form.valid && form.dirty) {
+
       let datasToApi = form.value
       datasToApi['userId'] = 1
       this.postApi(datasToApi)
@@ -30,23 +26,70 @@ export class TestformComponent implements OnInit {
   ngOnInit(): void {
     this.getApi()
   }
-  getApi(){
-    this.http.get("https://jsonplaceholder.typicode.com/posts").subscribe(
-      (res)=>{console.log(res),  this.list = res
-      
+  getApi() {
+    this.http.get("https://jsonplaceholder.typicode.com/comments").subscribe(
+      (res) => {
+        console.log(res), this.list = res
+
       },
-      (err)=>{console.warn(err)}
+      (err) => { console.warn(err) }
     )
   }
 
-  postApi(data){
-    console.log({'submiting':data})
-    this.http.post("https://jsonplaceholder.typicode.com/posts", data).subscribe(
-      (res)=>{console.log(res)
+  postApi(data) {
+    console.log({ 'submiting': data })
+    this.http.post("https://jsonplaceholder.typicode.com/comments", data).subscribe(
+      (res) => {
+        console.log(res)
         this.list.push(res)
       },
-      (err)=>{console.warn(err)}
+      (err) => { console.warn(err) }
     )
   }
+
+
+
+  saveFunction(data) {
+    
+    for (var i = 0; i < this.list.length; i++) {
+      if (this.list[i]["id"] == data.id) {
+        this.list.splice(i, 1, data);
+        i--;
+      }
+    }
+    console.log(this.list)
+  }
+
+  insertFunction(data) {
+    for (var i = 0; i < this.list.length; i++) {
+      if (this.list[i]["id"] == data.id) {
+        this.list.splice(i, 1, data);
+        i--;
+      }
+    }
+    console.log(this.list)
+  }
+
+
+
+
+  deteleFunction(id) {
+    console.log(id)
+
+    for (var i = 0; i < this.list.length; i++) {
+      if (this.list[i]["id"] == id) {
+        this.list.splice(i, 1);
+        i--;
+      }
+    }
+
+    // this.list.filter((item) => {
+    //   return item.id != id;
+    // })
+
+
+
+  }
+
 }
 
